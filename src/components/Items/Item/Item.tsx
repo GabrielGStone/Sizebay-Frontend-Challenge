c085d8880e0694218b2ed213aab0d3ac21acd3b0
GabrielStone <anjogabriel986@hotmail.com>
2024-02-28 00:00:03 -0300

    wip add edit task buttons

--- src/components/Items/Item/Item.tsx
+++ src/components/Items/Item/Item.tsx
@@ -1,9 +1,17 @@
-import { Box, Text } from "./styles";
+import { Box, Buttons, Circle, CompleteTask, DeleteTask, Text } from "./styles";
 
 const Item = () => {
   return (
     <Box>
       <Text>aaaaaaaaaaaaaaaaaaaaaaaa</Text>
+      <Buttons>
+        <DeleteTask>
+          <Circle>-</Circle>
+        </DeleteTask>
+        <CompleteTask>
+          <Circle>✓</Circle>
+        </CompleteTask>
+      </Buttons>
     </Box>
   );
 };
--- src/components/Items/Item/styles.ts
+++ src/components/Items/Item/styles.ts
@@ -23,16 +23,32 @@ export const Text = styled.div`
   padding: 10px 0 10px 16px;
 `;
 
-export const Add = styled.div`
+export const Buttons = styled.div`
+  display: flex;
+  height: 100%;
+  color: rgba(0, 0, 0, 0);
+`;
+export const DeleteTask = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
   width: 44px;
   height: 100%;
-  background-color: #4da6b3;
-  border: 1px solid #4da6b3;
+  background-color: #e34f4f;
+  font-weight: 400;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+export const CompleteTask = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 44px;
+  background-color: #5de290;
+  border: 1px solid #5de290;
   border-radius: 0 4px 4px 0;
-  color: #4da6b3;
   font-weight: 900;
   &:hover {
     cursor: pointer;
